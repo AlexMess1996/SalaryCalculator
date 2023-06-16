485bfb7c0924b8915989c88dfe21064ec11c5cf5
Norvigos <alexredfield13@gmail.com>
2023-06-16 23:51:53 +0200

    Able to go inside the ShowShifts menu and back to the main menu

--- src/Main.java
+++ src/Main.java
@@ -10,13 +10,44 @@ import static java.lang.System.exit;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) throws InterruptedException{
+    public static void main(String[] args) throws InterruptedException {
 
-        //Objects
+
         Scanner sc = new Scanner(System.in);
-        List<Shift> shifts = new ArrayList<>();
-        //variables
+        int option;
 
+        while (true) {
+            option = mainMenu();
+            clearConsole();
+
+            switch (option) {
+                case 1:
+                    registerMenu();
+                    break;
+                case 2:
+                    showShifts();
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid option. Please try again.");
+                    break;
+            }
+
+
+        }
+    }
+
+    private static void showShifts() {
+        clearConsole();
+        System.out.print("***********************\n" +
+                         "Historic of shifts\n" +
+                         "***********************");
+        displayShiftsDataFromFile();
+    }
+
+    public static void registerMenu() throws InterruptedException {
         //User input variables UI
         double UI_amountOrdersWeekday = 0;
         double UI_amountOrdersSaturday = 0;
@@ -26,63 +57,19 @@ public class Main {
         double UI_totalTips = 0;
         double UI_workedHours = 0;
 
-        //if the user decides to register data
-        char registerMenu = ' '; // Y or N (y or n)
-        int chooseRegister = 0; // 1 or 2. 1 to register new shift or 2 to go back to the main menu.
+        List<Shift> shifts = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
 
-        //if the user decides to read data from the .txt file
-        int type = 0; //1: daily , 2: weekly, 3: monthly
-        int chooseSum = 0; // 1 or 2. 1 to go back into choosing the timeframe or 2 to go back to the main menu
-
-
-        //constant variables
-        double baseHourlySalary = 134.39;
-        double priceOrderWeekday = 17.25;
-        double priceOrderSaturday = 22.51;
-        double priceOrderSunday = 27.77;
-        double pricePerKm = 2.067;
-        double guaranteePayWeekday = 152.28;
-        double guaranteePaySaturday = 162.8;
-        double guaranteePaySunday = 173.32;
-
-        //dynamic variables
-        double amountOrdersWeekday = 0;
-        double amountOrdersSaturday = 0;
-        double amountOrdersSunday = 0;
-        double seniority = 0;
-        double kmPerOrder = 0;
-        double tipsPerOrder = 0;
-        double yearsOfService = UI_seniority;
-
-        //sum variables
-        double sumTips = 0;
-        double priceKmPerOrder = 0;
-
-        //calculations
-        priceOrderWeekday *=amountOrdersWeekday;
-        priceOrderSaturday *=amountOrdersSaturday;
-        priceOrderSunday *=amountOrdersSunday;
-        sumTips +=tipsPerOrder;
-        seniority +=yearsOfService;
-        priceKmPerOrder = pricePerKm * kmPerOrder;
-
-        //for weekdays
-        //title
-        clearConsole();
-
-        switch (mainMenu()) {
-            case 1 -> {
-                clearConsole();
-                String storeDate;
-                String storeTypeofWeek;
-                double storeWorkedHours;
-                double storeKm;
-                double storeTips;
-                double storeSeniority;
+        String storeDate;
+        String storeTypeofWeek;
+        double storeWorkedHours;
+        double storeKm;
+        double storeTips;
+        double storeSeniority;
 
                 System.out.println("***********************\n" +
-                        "Shift Register\n" +
-                        "***********************\n");
+                                   "Shift Register\n" +
+                                   "***********************\n");
                 Thread.sleep(1500);
                 System.out.print("Today's date (dd.mm.yyyy): " +
                         "Your input: ");
@@ -90,26 +77,26 @@ public class Main {
                 date = sc.next();
                 storeDate = date;
                 System.out.print("\nChoose the following about your shift:(1,2 or 3) \n" +
-                        "1: Weekday\n" +
-                        "2: Saturday\n" +
-                        "3: Sunday\n" +
-                        "Your input: ");
+                                 "1: Weekday\n" +
+                                 "2: Saturday\n" +
+                                 "3: Sunday\n" +
+                                 "Your input: ");
                 int typeOfWeek = sc.nextInt();
                 String typeofWeek = "";
                 if(typeOfWeek == 1){
-                     typeofWeek = "Weekday";
+                    typeofWeek = "Weekday";
 
                 }else if (typeOfWeek == 2){
-                     typeofWeek = "Saturday";
+                    typeofWeek = "Saturday";
                 }else if (typeOfWeek == 3){
-                     typeofWeek = "Sunday";
+                    typeofWeek = "Sunday";
                 }
                 storeTypeofWeek = typeofWeek;
 
                 System.out.print("\nAmount of hours worked: " +
                         "\nYour input: ");
-                        UI_workedHours = sc.nextDouble();
-                        storeWorkedHours = UI_workedHours;
+                UI_workedHours = sc.nextDouble();
+                storeWorkedHours = UI_workedHours;
                 System.out.println("\nDid you have a moment where you didn't have any orders? (Y/N): ");
                 String guaranteeChoice = "";
                 guaranteeChoice = sc.next();
@@ -129,52 +116,50 @@ public class Main {
                 shifts.add(new Shift(storeDate,storeTypeofWeek,storeWorkedHours,storeKm,
                         storeTips,storeSeniority));
 
-                 clearConsole();
+                clearConsole();
                 loadingScreen();
                 Thread.sleep(1400);
                 clearConsole();
 
                 System.out.print("**********************************************\n" +
-                        "Date: "+date+"\n" +
-                         "\nType of the week: "+typeofWeek+"\n" +
-                        "\nHours worked: "+UI_workedHours+"\n" +
-                        "\nDid you have a moment where you didn't work?: "+guaranteeChoice+"\n" +
-                        "\nKm in total: "+UI_totalKm+"\n" +
-                        "\nTips: "+ UI_totalTips+"\n" +
-                        "\nYears worked: "+UI_seniority+"\n" +
-                        "**********************************************\n");
+                                 "Date: "+date+"\n" +
+                                 "\nType of the week: "+typeofWeek+"\n" +
+                                 "\nHours worked: "+UI_workedHours+"\n" +
+                                 "\nDid you have a moment where you didn't work?: "+guaranteeChoice+"\n" +
+                                 "\nKm in total: "+UI_totalKm+"\n" +
+                                 "\nTips: "+ UI_totalTips+"\n" +
+                                 "\nYears worked: "+UI_seniority+"\n" +
+                                 "**********************************************\n");
 
+                Thread.sleep(1400);
                 System.out.print("\n\nSubmit? (Y/N) " +
-                        "\nYour input: ");
+                                 "\nYour input: ");
                 String choose = sc.next();
 
                 if(Objects.equals(choose, "N") || Objects.equals(choose, "n")){
-                    exit(0);
+                    clearConsole();
+                    registerMenu();
                 }else if(Objects.equals(choose, "Y") || Objects.equals(choose, "y")){
+                    Thread.sleep(1000);
                     saveShiftsDataToFile(shifts);
                 }
-
+                //clearConsole();
             }
-            case 2 ->{
-                clearConsole();
-                System.out.print("***********************\n" +
-                        "Historic of shifts\n" +
-                        "***********************");
 
-                displayShiftsDataFromFile();
-            }
-        }
 
-    }
+
 
     private static void displayShiftsDataFromFile() {
         try(BufferedReader reader = new BufferedReader(new FileReader("shifts.txt"))){
+            Scanner sc = new Scanner(System.in);
+            Thread.sleep(1000);
             System.out.println("\nShifts datatable");
             System.out.println("--------------------------------------------------------------------------------------------------");
             System.out.printf("| %-20s | %-20s | %-8s | %-8s | %-8s | %-8s |%n", "Date", "Type", "Hours", "Km","Tips","Seniority");
             System.out.println("--------------------------------------------------------------------------------------------------");
-
             String line;
+            boolean choice = false;
+
             while ((line = reader.readLine()) != null){
                 String[] data = line.split(",");
                 if(data.length == 6){
@@ -190,7 +175,13 @@ public class Main {
                 }
             }
             System.out.println("--------------------------------------------------------------------------------------------------");
-        } catch (IOException e){
+            Thread.sleep(1000);
+            waitForEnter();
+            choice = true;
+            if(choice){
+                clearConsole();
+            }
+        } catch (IOException | InterruptedException e){
             System.out.println("An error occurred while reading the file.");
         }
     }
@@ -229,22 +220,29 @@ public class Main {
     }
 
     public static int mainMenu() throws InterruptedException {
+        clearConsole();
         Scanner sc = new Scanner(System.in);
-        int result = 0;
-        System.out.print(
-                        "***********************\n"+
-                        "Salary Calculator 2023\n" +
-                        "***********************\n");
-        Thread.sleep(1200);
-        //main menu
-        System.out.print(
-                        "1: Register\n"+
-                        "2: Show sum\n"+
-                        "3: Exit\n\n"  +
-                        "Your input: ");
+        int result;
+
+        System.out.println("***********************");
+        System.out.println("Salary Calculator 2023");
+        System.out.println("***********************");
+        System.out.println("1: Register");
+        System.out.println("2: Show shifts");
+        System.out.println("3: Exit");
+        System.out.print("Your input: ");
         result = sc.nextInt();
+
         return result;
     }
+
+    public static void waitForEnter(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nPress Enter to go back to the Main Menu...");
+        sc.nextLine();
+
+    }
+
     private static class Shift{
         private String date;
         private String typeOfWeek;
